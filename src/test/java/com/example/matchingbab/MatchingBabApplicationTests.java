6dd17958df3a051f7744e34054e4d55a51ff74f7
이름 <>
2026-06-17 16:03:14 +0900

    [BE-001] Spring Boot 프로젝트 초기 세팅 + DB + Swagger

---
+++ src/test/java/com/example/matchingbab/MatchingBabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matchingbab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingBabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
